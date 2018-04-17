67f6390bf3cf95154991d26eb1cd944ef036d58e
Sheryl Hohman <sherylhohman@yahoo.com>
2018-04-17 14:03:43 -0700

    feat: add StatusBar
    	modified:   App.js

--- App.js
+++ App.js
@@ -1,11 +1,40 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { Constants } from 'expo';
+
+function AppStatusBar({ backgroundColor, ...props }){
+  // backgroundColor blends with default statusbar color;
+  //  default statusbar color is..
+  //  on ios:  white => statusBarColor === background color
+  //  android: gray  => statusBarColor a Darker Shade of background color
+  return (
+    <View style={{
+            backgroundColor,
+            height: Constants.statusBarHeight,
+          }}
+      >
+      <StatusBar
+        translucent
+        backgroundColor={backgroundColor}
+        {...props}
+      />
+    </View>
+  );
+}
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Welcome to UdaciCards-sh !</Text>
+      <View style={{flex:1}}>
+        <AppStatusBar
+          backgroundColor="orange"
+          barStyle="light-content"
+          />
+
+        <View style={styles.container}>
+          <Text>Welcome to UdaciCards !</Text>
+          <Text> by Sheryl Hohman </Text>
+        </View>
       </View>
     );
   }
