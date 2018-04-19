e9b3dfba8268242f4255bde4a161c143fc288d3a
Sheryl Hohman <sherylhohman@yahoo.com>
2018-04-18 17:34:20 -0700

    feat: nieve rendering of Quiz Deck Titles
    	modified:   components/DeckList.js

--- components/DeckList.js
+++ components/DeckList.js
@@ -8,22 +8,55 @@ import { white, primaryColor, primaryColorDark } from '../utils/colors';
 import { fetchDecks } from '../utils/api';
 
 export default class DeckList extends React.Component {
-  componentDidMount(){
-    const { dispatch } = this.props;
 
+  // temp
+  state = {
+    decks: null,
+  }
+
+  componentDidMount(){
     fetchDecks()
       .then( (appData) => {
         console.log('appData:', appData);
+
+        // temp so I can render/see the data
+        if (appData && (typeof appData === 'object') && !Array.isArray(appData)) {
+          const decks = Object.keys(appData).reduce((acc, id) => {
+            return acc.concat([{ id, title: appData[id].title }]);
+          }, []);
+          this.setState({ decks });
+        }
+
+      })
+      .catch((err) => {
+        console.log('error fetching data in DeckList, cDM, err:', err);
       })
   }
+
   render() {
+    const decks = this.state.decks;
+
+    if (!this.state.decks || !(this.state.decks.length > 0)) {
+      // TODO: proper loading message/spinner that resolves on fetch failure
+      return (
+        <Text> Fetching Quiz Decks </Text>
+      );
+    }
+
     return (
       <View style={styles.container}>
           <Text>Welcome to UdaciCards !!</Text>
           <Text> by Sheryl Hohman </Text>
           <Text> {"\n\n"} </Text>
 
-          <Text> Deck List Page </Text>
+          <Text> Available Quiz Decks: </Text>
+          {decks.map((deck) => {
+            return (
+              <View key={deck.id} style={styles.item}>
+                <Text>{deck.title}</Text>
+              </View>
+            )
+          })}
           <Deck />
           <Quiz />
         </View>
@@ -37,5 +70,9 @@ const styles = StyleSheet.create({
     backgroundColor: white,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  item: {
+    margin: 10,
+    backgroundColor: 'yellow',
   },
 });
