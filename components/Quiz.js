580fa8ce3f544563499c271eddbcc122ff550723
Sheryl Hohman <sherylhohman@yahoo.com>
2018-04-20 18:04:59 -0700

    feat: UI and styling for Quiz
      - Displays a single card, user interface buttons, progress bar.
    
      - Button enabled to switch between Question and Answer.
    
      - TODO: enable other buttons, and
              add logic to progress through all cards in the deck.
    
      - TODO: write UI for completed Quiz
              write UI for errors (ie if do not have a quiz deck)
    
    	modified:   components/Quiz.js

--- components/Quiz.js
+++ components/Quiz.js
@@ -1,22 +1,233 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { white, primaryColor, primaryColorDark } from '../utils/colors';
+import { connect } from 'react-redux';
+import { View, Text, TouchableOpacity,
+         StyleSheet, Platform ,
+       } from 'react-native';
+// Components
+import StyledButton from '../components/StyledButton';
+// actionCreators, reducers, selectors
+import { getDeck, getCards } from '../store/decks/selectors';
+import {
+  white, gray, primaryColor, primaryColorDark, isCorrectColor, isIncorrectColor,
+} from '../utils/colors';
 
-export default class Quiz extends React.Component {
+class Quiz extends React.Component {
+
+  static navigationOptions = ({ navigation }) => {
+    // TODO: use the actual title instead of id
+    // TODO: can I also put current card index here too? I think not, but verify..
+    const { id } = navigation.state.params;
+    return { title: `${id} Quiz`}
+  }
+
+  state = {
+    isQuestionView: true,
+    index: 0,
+    numCorrect: 0,
+  }
+
   render() {
+    const { title, id, questions } = this.props.deck;
+    const numCards = questions.length;
+    const { index, isQuestionView } = this.state;
+
+    // show error message
+    if (false){
+      // TODO
+    }
+
+    // show quiz results
+    if (index >= this.props.numcards){
+      // TODO
+    }
+
+    // show quiz
+    // TODO: wrapped long lines of txt (long Questions/Answers) do NOT center !
     return (
       <View style={styles.container}>
-        <Text>Quiz for deck: {"<TBA>"}</Text>
+
+        <View style={styles.everythingExceptProgressBar}>
+          <View style={styles.cardContainer}>
+            <View style={styles.label}>
+              {isQuestionView
+                /* Heading: "Question" or "Answer" */
+                  ? <Text style={[styles.infoText,{color: primaryColorDark}]}>Question:</Text>
+                  : <Text style={[styles.infoText,{color: primaryColorDark}]}>Answer:</Text>
+              }
+            </View>
+
+            <View>
+              {isQuestionView
+                /* The Question or The Answer */
+                ? <Text style={styles.titleText}>{questions[index].question}</Text>
+                : <Text style={styles.titleText}>{questions[index].answer}</Text>
+              }
+            </View>
+          </View>
+
+          <View style={styles.buttonsContainer}>
+            <TouchableOpacity
+              onPress={() => {this.setState({ isQuestionView: !isQuestionView })}}
+              style={styles.label}
+              >
+              {isQuestionView
+                /* button to show The Question, or The Answer */
+                ? <Text style={[styles.infoText,{color: primaryColorDark}]}>(view Answer)</Text>
+                : <Text style={[styles.infoText,{color: primaryColorDark}]}>(view Question)</Text>
+              }
+            </TouchableOpacity>
+
+            {/* Mark as Correct */}
+            <View style={styles.buttonContainer}>
+              <StyledButton
+              onPress={() => {}}
+              customColor={isCorrectColor}
+              >
+              I Got This Right !!
+              </StyledButton>
+            </View>
+
+            {/* Mark as InCorrect */}
+            <View style={styles.buttonContainer}>
+              <StyledButton
+              onPress={() => {}}
+              customColor={isIncorrectColor}
+              >
+              I need to Study This One
+              </StyledButton>
+            </View>
+          </View>
+        </View>
+
+        {/* Quiz Progress Bar */}
+        <View style={styles.progressBarContainer}>
+          <Text style={styles.infoText}>{index}/{numCards}</Text>
+        </View>
+
       </View>
     );
   }
 }
+
+const testing = false;
+const borderWidth = testing ? 2 : 0;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: white,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
+    padding: 10,
+    paddingTop: 30,
+    paddingBottom: 5,
+    //TEMP
+    borderColor: 'red',
+    borderWidth,
   },
+  everythingExceptProgressBar: {
+    flex: 1,
+    alignSelf: 'stretch',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: white,
+    //TEMP
+    borderColor: 'red',
+    borderWidth,
+  },
+
+  // item: {
+  cardContainer: {
+    flex: 8,
+    justifyContent: 'flex-start',
+    alignSelf: 'stretch',
+    // alignItems: 'center',
+    alignItems: 'stretch',
+    backgroundColor: '#fefefe',
+
+    padding:     20,
+    marginLeft:  30,
+    marginRight: 30,
+    marginTop:   10,
+    borderRadius: Platform.OS === 'ios' ? 20 : 10,
+
+    shadowRadius: 3,
+    shadowOpacity: 0.8,
+    shadowColor: 'rgba(0, 0, 0, 0.24)',
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    marginBottom:20,
+    //TEMP:
+    borderColor: 'red',
+    borderWidth,
+  },
+  titleText: {
+    fontSize: 27,
+    color: primaryColor,
+    alignSelf: 'center',
+    flexWrap: 'wrap',
+    textAlign: 'center',
+    // Styling on android buttons uses colored backgrounds.
+    // These large blocks of solod color cause eyes to be drawn naturally to the buttons.
+    // Questions/Answers, *should* take the users/eyes main focus.
+    // make Q/A text bolder on android, to minimize this competition.
+    // Eyes now naturally drawn to Q/A, rather than buttons !
+    fontWeight: Platform.OS === 'ios' ? 'normal' : 'bold',
+    //TEMP:
+    borderColor: 'blue',
+    borderWidth,
+  },
+  infoText: {
+    fontSize: 20,
+    color: gray,
+    alignSelf: 'center',
+    //TEMP:
+    borderColor: 'blue',
+    borderWidth,
+  },
+
+  buttonsContainer: {
+    flex: 10,
+    alignSelf: 'stretch',
+    justifyContent: 'flex-start',
+    //TEMP:
+    borderColor: 'red',
+    borderWidth,
+  },
+  label: {
+    marginBottom: 20,
+    //TEMP:
+    borderColor: 'red',
+    borderWidth,
+  },
+  buttonContainer: {
+    justifyContent: 'center',
+    margin: 10,
+    //TEMP:
+    borderColor: 'red',
+    borderWidth,
+  },
+
+  progressBarContainer: {
+    height: 30,
+    flexDirection: 'row',
+    //TEMP:
+    borderColor: 'red',
+    borderWidth,
+  },
+  // TODO: Progress Box (numCards Boxes. cardIndex+1 boxes filled in)
+  // progressBox: {},
 });
+
+function mapStoreToProps(store, ownProps){
+  console.log(ownProps);
+  const deck  = getDeck(store, ownProps.navigation.state.params.id) || null;
+
+  return {
+    deck,
+  }
+}
+
+export default connect(mapStoreToProps)(Quiz);
